b0a8ce2df86c91fb3b360bb1f9d67909c5046af0
Drew Atkin <datkin@gmail.com>
2016-04-17 21:32:56 -0400

    simple example working

--- server_stubs.c
+++ server_stubs.c
@@ -1,23 +1,33 @@
 #include <util.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 char* argv[] = { "ls" };
+char* envp[] = { "TERM=xterm" };
 
 int main(int argc, char** argv) {
-  execve("/bin/ls", argv, NULL);
-  /*
-  int* stat_loc;
-  int* master_fd;
-  //char* arg0 = "ls";
-  pid_t pid = forkpty(master_fd, NULL, NULL, NULL);
+  int master_fd = -1;
+  pid_t pid = forkpty(&master_fd, NULL, NULL, NULL);
   if (pid == 0) {
-    execve("/bin/ls", argv, NULL);
+    execve("/usr/bin/clear", argv, envp);
   } else {
+    int* stat_loc = NULL;
+    size_t buf_size = 1024;
+    char* buf = malloc(buf_size);
+    size_t size_read = 0;
+    unsigned int total = 0;
+    while ((size_read = read(master_fd, buf, buf_size)) > 0) {
+      for (int i = 0; i < size_read; i++) {
+        if (total % 10 == 0) { printf("\n"); }
+        printf(" %02x (%c)", buf[i], buf[i]);
+        total++;
+      }
+    }
+    printf("\ndone\n");
     waitpid(pid, stat_loc, 0);
     return 0;
   }
-  */
 }
