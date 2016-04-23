8e5b22bc02f7a853b2ae923262ddcbfd2fb22f6d
Drew Atkin <datkin@gmail.com>
2016-04-22 23:16:23 -0400

    set

--- server/window.ml
+++ server/window.ml
@@ -37,23 +37,53 @@ let invariant t =
 
 let set_dimensions t dim = t.dim <- dim
 
-let update _t _buf = assert false
-
-let cursor t = t.cursor
-
 let null_byte = Char.of_int_exn 0
+
+let select_row_exn t y =
+  let rec loop y rows =
+    match t.num_rows - y, rows with
+    | 0, row :: _ -> row
+    | n, _ :: rows -> loop (y-1) rows
+    | _, [] -> assert false
+  in
+  assert (y >= 0);
+  assert (y < t.num_rows);
+  loop y t.rows
+
+(* Just set the character at the given coordinate. *)
+let set t { x; y; } chr =
+  if y >= t.num_rows
+  then ()
+  else
+    let row = select_row_exn t y in
+    if x >= Array.length row
+    then ()
+    else Array.set row x chr
 
 let get t { x; y; } =
   if y >= t.num_rows
   then null_byte
   else
-    let rec select_row y rows =
-      match t.num_rows - y, rows with
-      | 0, row :: _ -> row
-      | n, _ :: rows -> select_row (y-1) rows
-      | _, [] -> assert false
-    in
-    let row = select_row y t.rows in
+    let row = select_row_exn t y in
     if x >= Array.length row
     then null_byte
     else Array.get row x
+
+(* CR datkin: What happens at out of bounds? *)
+let incr { x; y; } { width; height = _; } =
+  let next = (y * width) + x + 1 in
+  let y = next / width in
+  let x = next mod width in
+  { x; y; }
+
+(* Write char to the current cursor and move the cursor. *)
+let putc t chr =
+  assert false
+
+let update t buf =
+  let rec copy i =
+    assert false
+  in
+  copy 0
+
+let cursor t = t.cursor
