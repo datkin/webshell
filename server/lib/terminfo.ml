807ebbaf79402148b677231d2464a142c9f7a599
Drew Atkin <datkin@gmail.com>
2016-05-15 21:10:19 -0400

    (some) terminfo escaping

--- server/lib/terminfo.ml
+++ server/lib/terminfo.ml
@@ -25,9 +25,69 @@ let add_alias t alias =
 
 let add t key data = { t with capabilities = Map.add t.capabilities ~key ~data; }
 
+let parse_str =
+  let unescape_caret =
+    String.Escaping.unescape_gen_exn
+      ~escape_char:'^'
+      ~escapeworthy_map:[
+        (* From Wikipedia's "ASCII control code chart" *)
+        '\000', '@';
+        '\001', 'A';
+        '\002', 'B';
+        '\003', 'C';
+        '\004', 'D';
+        '\005', 'E';
+        '\006', 'F';
+        '\007', 'G';
+        '\010', 'H';
+        '\011', 'I';
+        '\012', 'J';
+        '\013', 'K';
+        '\014', 'L';
+        '\015', 'M';
+        '\016', 'N';
+        '\017', 'O';
+        '\020', 'P';
+        '\021', 'Q';
+        '\022', 'R';
+        '\023', 'S';
+        '\024', 'T';
+        '\025', 'U';
+        '\026', 'V';
+        '\027', 'W';
+        '\030', 'X';
+        '\031', 'Y';
+        '\032', 'Z';
+        '\033', '[';
+        '\034', '\\';
+        '\035', ']';
+        (*'\036', '^';*)
+        '\037', '_';
+        '\127', '?';
+      ]
+    |> unstage
+  in
+  let unescape_slash =
+    String.Escaping.unescape_gen_exn
+      ~escape_char:'\\'
+      ~escapeworthy_map:[
+        (*'\033', 'e';*)
+        '\033', 'E';
+      ]
+    |> unstage
+  in
+  fun data ->
+    unescape_caret (unescape_slash data)
+
+let%test_unit _ =
+  [%test_result: string]
+    (parse_str "\E^X")
+    ~expect:"\033\030"
+;;
+
 let parse_entry entry =
   match String.lsplit2 entry ~on:'=' with
-  | Some (key, value) -> Ok (key, String value)
+  | Some (key, value) -> Ok (key, String (parse_str value))
   | None ->
     match String.lsplit2 entry ~on:'#' with
     | Some (key, value) ->
@@ -40,7 +100,6 @@ let parse_entry entry =
   * \E and \e -> 
   * \, and \054 -> ,
   * any octal \<NNN> seq
-  * ^<x> -> control-<x> for all x
   *)
 
 let parse raw =
@@ -51,11 +110,10 @@ let parse raw =
     |> List.map ~f:String.strip
     |> String.concat ~sep:" "
   in
-  (* CR datkin: Literal ','s are either '\,' or the octal escape sequence, \054. *)
-  (* CR datkin: Stripping another oter than a leading space on the front may be
+  (* CR datkin: Stripping another other than a leading space on the front may be
    * too much. *)
   match
-    String.split db ~on:','
+    String.Escaping.split db ~escape_char:'\\' ~on:','
     |> List.map ~f:String.strip
     |> List.filter ~f:(fun x -> not (String.is_empty x))
   with
@@ -71,6 +129,7 @@ let parse raw =
           List.fold entries ~init:t ~f:(fun t entry ->
             match parse_entry entry with
             | Error _ as err -> return err
+            | Ok ("acsc", _) (* This cap is different. *)
             | Ok ("rmacs", _)
             | Ok ("home", _) -> t (* Hack for now, b/c home and clear overlap in xterm *)
             | Ok (key, data) -> add t key data)
