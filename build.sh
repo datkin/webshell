a60206c3ff441de61d1d089cf2dcfe2c7247f314
Drew Atkin <datkin@gmail.com>
2017-02-08 21:04:54 -0500

    building

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 set -o errexit
+set -x
 
 opam switch 4.03.0 && eval $(opam config env)
 
+# -I odditty_kernel \
 # CR datkin: Pass different ppx flags for inline test runner and main.
 ocamlbuild \
-  -I odditty_kernel \
   -use-ocamlfind \
   -pkg core_kernel \
   -pkg async_kernel \
@@ -17,6 +18,21 @@ ocamlbuild \
   -cflags -w,+a-40-42-44 \
   odditty_kernel/odditty_kernel.cmxa \
   odditty_kernel/inline_test_runner.native
+
+# CR datkin: Pass different ppx flags for inline test runner and main.
+ocamlbuild \
+  -use-ocamlfind \
+  -I odditty_kernel \
+  -pkg core \
+  -pkg async \
+  -pkg ppx_expect \
+  -pkg ppx_expect.evaluator \
+  -tag thread \
+  -tag 'ppx(ppx-jane -as-ppx -inline-test-lib odditty)' \
+  -cflags -w,+a-40-42-44 \
+  -cflags -cclib,-lodditty_stubs \
+  -verbose 3 \
+  odditty/odditty.cmxa \
 
 ./inline_test_runner.native \
   inline-test-runner \
